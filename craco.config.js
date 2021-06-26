c8d6e0010bcdd51b733939f6e470a1fb216e5401
ArnoldDadayan <dadayanaa@yahoo.com>
2021-06-26 15:35:00 +0400

    [dev] craco config new path @components

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
+      '@components': path.resolve(__dirname, "src/components"), 
       '@modules': path.resolve(__dirname, "src/modules"),
       '@config': path.resolve(__dirname, "src/config")
     }
