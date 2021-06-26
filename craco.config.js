2a69369e2a3f1927c38755b5848b76ab7751f00a
ArnoldDadayan <dadayanaa@yahoo.com>
2021-06-26 21:33:30 +0400

    [dev] new alias paths service, utilities

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,9 @@ module.exports = {
     alias: {
       '@components': path.resolve(__dirname, "src/components"), 
       '@modules': path.resolve(__dirname, "src/modules"),
-      '@config': path.resolve(__dirname, "src/config")
+      '@config': path.resolve(__dirname, "src/config"),
+      '@services': path.resolve(__dirname, "src/services"),
+      '@utilities': path.resolve(__dirname, "src/utilities"),
     }
   }
 }
