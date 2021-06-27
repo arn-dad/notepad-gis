4819764131e1907d512f5b899af11b713076bb08
arn-dad <dadayanaa@yahoo.com>
2021-06-27 13:31:34 +0400

    [fix] removed unused import

--- src/modules/CreateNotepad/components/CreateNotepadForm/CreateNotepadForm.jsx
+++ src/modules/CreateNotepad/components/CreateNotepadForm/CreateNotepadForm.jsx
@@ -3,7 +3,6 @@ import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import IconButton from '@material-ui/core/IconButton';
