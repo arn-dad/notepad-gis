4c11ff09cf4db19e6b36d450cdcd0108c92d74bb
arn-dad <dadayanaa@yahoo.com>
2021-06-27 13:30:37 +0400

    [fix] validation message changes

--- src/modules/CreateNotepad/schema/notepad.schema.js
+++ src/modules/CreateNotepad/schema/notepad.schema.js
@@ -1,18 +1,21 @@
 import * as Yup from "yup";
 
+// @TODO empty string validation
+
 export const notepadSchema = Yup.object().shape({
   title: Yup
-    .string('Notepad title is required')
-    .max(255, 'Title should be of maximum 255 characters length')
-    .required('Notepad Title is required'),
+    .string('Title is required')
+    .max(255, 'Title should be of maximum 255 characters length.')
+    .required('Title is required.'),
   notes: Yup.array().of(
     Yup.object().shape({
       title: Yup
-        .string().required("Note title is required")
-        .max(255, 'Title should be of maximum 255 characters length'),
+        .string('Title is required.')
+        .required("Title is required.")
+        .max(255, 'Title should be of maximum 255 characters length.'),
       note: Yup
-        .string().required("Note is required")
-        .max(1000, 'Note should be of maximum 1000 characters length'),
+        .string().required("Note is required.")
+        .max(1000, 'Note should be of maximum 1000 characters length.'),
     })
   )
 });
